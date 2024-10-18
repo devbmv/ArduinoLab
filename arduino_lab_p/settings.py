9421eec8959658fc10717e8b373aab3a5d3144c0
devbmv <maik_775@yahoo.com>
2024-10-18 09:57:50 +0100

    Update settings file'

--- arduino_lab_p/settings.py
+++ arduino_lab_p/settings.py
@@ -18,17 +18,25 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if os.path.exists('env.py'):
     print('.env file found')
     import env  # Import only if .env exists# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
+def debug(data):
+    if os.getenv('show_env_vars', 'False') == 'True':
+        print(data)
+        
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-if(DEBUG):
-    print("Debug mode is on")
+if DEBUG:
+    print('Running in DEBUG mode')
+
+debug(DEBUG)
+
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
+debug(f"CSRF_TRUSTED_ORIGINS={CSRF_TRUSTED_ORIGINS}")
+debug(f"ALLOWED_HOSTS={ALLOWED_HOSTS}")
 
 # Application definition
 
@@ -57,6 +65,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Adaugă acest middleware
     'allauth.account.middleware.AccountMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,7 +142,7 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+debug(f"DATABASES={DATABASES}")
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -166,17 +175,17 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+ 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 if 'USE_AWS' in os.environ:
     # Cache control
@@ -184,6 +193,8 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
+    debug(f"AWS_SECRET_ACCESS_KEY={os.environ.get('USE_AWS')}")
+
 
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')  # Preia numele bucket-ului S3 din variabila de mediu
@@ -192,9 +203,14 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')  # Secretul luat din env.py
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
+    debug(f"AWS_STORAGE_BUCKET_NAME={AWS_STORAGE_BUCKET_NAME}")
+    debug(f"AWS_S3_REGION_NAME={AWS_S3_REGION_NAME}")
+    debug(f"AWS_ACCESS_KEY_ID={AWS_ACCESS_KEY_ID}")
+    debug(f"AWS_SECRET_ACCESS_KEY={AWS_SECRET_ACCESS_KEY}")
+
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
+    STATICFILES_LOCATION = 'staticfiles'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
@@ -211,16 +227,23 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+debug(f"STRIPE_WH_SECRET={STRIPE_WH_SECRET}")
+debug(f"STRIPE_PUBLIC_KEY={STRIPE_PUBLIC_KEY}")
+debug(f"STRIPE_SECRET_KEY={STRIPE_SECRET_KEY}")
+
 if os.environ.get('DEBUG', 'False') == 'True':
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'maik_775@yahoo.com'
+    DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', 'noreply@example.com')
+    debug(f"DEFAULT_FROM_EMAIL={DEFAULT_FROM_EMAIL}")
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST = 'smtp.mail.yahoo.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
- 
+    DEFAULT_FROM_EMAIL =EMAIL_HOST_USER
+    debug(f"EMAIL_HOST_USER={EMAIL_HOST_USER}")
+    debug(f"EMAIL_HOST_PASS={EMAIL_HOST_PASSWORD}")
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
