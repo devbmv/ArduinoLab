fae7f4c4a6b729463c54b30b9c55bcb7e5f34890
devbmv <maik_775@yahoo.com>
2024-10-24 23:27:58 +0100

    Try to fix collectstatic from AWS

--- custom_storages.py
+++ custom_storages.py
@@ -1,14 +1,9 @@
-from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
-    default_acl = 'public-read'  # Asigură acces public la fișierele media
-
-
+    location = "static"  # Locația fișierelor statice în bucket-ul S3
+    default_acl = 'public-read'  # Asigură acces public la fișierele statice
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    location = "media"  # Locația fișierelor media în bucket-ul S3
     default_acl = 'public-read'  # Asigură acces public la fișierele media
-
